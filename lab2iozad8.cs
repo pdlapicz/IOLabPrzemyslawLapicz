d7166b02199691307409ae85e7c87bb8e14c24e3
Przemyslaw Lap <przemyslaw.lapicz@gmail.com>
2017-11-07 14:23:31 +0100

    Create lab2iozad8.cs

---
+++ lab2iozad8.cs
@@ -0,0 +1,21 @@
+//Autor: Przemyslaw Lapicz
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*ZADANIE 8
+Napisz program, który asynchronicznie wywoła metody rekurencyjnego 
+oraz iteracyjnego obliczania silni oraz kolejnych elementów ciągu Fibonacciego. 
+Sprawdź, które metody zostaną wykonane jako pierwsze. W swoim rozwiązaniu użyj
+metod BeginInvoke, EndInvoke oraz delegatów. Samodzielnie dobierz tryb APM. */
+
+namespace lab2iozad8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
